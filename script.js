48bc3d40b8595b81851e50c85956617399e0ec46
Sean Mafnas <seanvincent671@gmail.com>
2025-07-11 22:33:48 +1000

    Add password length and character options; update styles and functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const password1El = document.querySelector(".password-1");
-const password2El = document.querySelector(".password-2");
+const password1El = document.querySelector(".password1");
+const password2El = document.querySelector(".password2");
 const generateBtnEl = document.querySelector(".context__generate-btn");
+const lengthEl = document.querySelector("#length");
+const includeUppercaseInput = document.querySelector("#include-uppercase");
+const includeLowercaseInput = document.querySelector("#include-lowercase");
+const includeNumbersInput = document.querySelector("#include-numbers");
+const includeSymbolsInput = document.querySelector("#include-symbols");
 
 const characters = [
   "A",
@@ -96,6 +101,53 @@ const characters = [
   "/",
 ];
 
-for (let i = 0; i < characters.length; i++) {
-  let randomIndex = Math.floor(Math.random() * characters.length);
+// Function to get filtered characters based on user selections
+function getFilteredCharacters() {
+  let filteredChars = [];
+  if (includeUppercaseInput.checked) {
+    filteredChars.push(...characters.filter((char) => /[A-Z]/.test(char)));
+  }
+  if (includeLowercaseInput.checked) {
+    filteredChars.push(...characters.filter((char) => /[a-z]/.test(char)));
+  }
+  if (includeNumbersInput.checked) {
+    filteredChars.push(...characters.filter((char) => /[0-9]/.test(char)));
+  }
+  if (includeSymbolsInput.checked) {
+    filteredChars.push(
+      ...characters.filter((char) =>
+        /[~`!@#$%^&*()_+\-={}\[\]|\\:;"'<>,.?/]/.test(char)
+      )
+    );
+  }
+  // If no filters are omitted, use all characters
+  if (filteredChars.length === 0) {
+    filteredChars = characters;
+  }
+  return filteredChars;
 }
+
+// Function to generate a random character from the filtered character array
+function generateRandomChar(charArray) {
+  let randomIndex = Math.floor(Math.random() * charArray.length);
+  return characters[randomIndex];
+}
+
+// Function to generate a password based on the selected length and character filters
+function generatePassword() {
+  const length = parseInt(lengthEl.value) || 15;
+  const charArray = getFilteredCharacters();
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += generateRandomChar(charArray);
+  }
+  return password;
+}
+
+// Function to handle the click event for generating passwords
+function handleClick() {
+  password1El.textContent = generatePassword();
+  password2El.textContent = generatePassword();
+}
+
+generateBtnEl.addEventListener("click", handleClick);
